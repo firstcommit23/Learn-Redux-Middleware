b75c371cf9ffece8e7da004856e8209cc4639b5b
jimin <jm131313.kim@gmail.com>
2021-10-09 01:42:08 +0900

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import SampleContainer from './containers/SampleContainer';
+import CounterContainer from './containers/CounterContainer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SampleContainer />
     </div>
   );
 }
